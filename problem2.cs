ea3aa2fbc9aab071f4ed37ff14b92c4e4d8b84e7
Andrew Taylor <rolyata47@gmail.com>
2022-06-27 14:16:55 -0500

    Improving algorithm

--- problem2.cs
+++ problem2.cs
@@ -55,6 +55,7 @@ public class Solution {
                 l1 = l1.next;
             if (l2 != null)
                 l2 = l2.next;
+                
             if (l1 == null && l2 == null && carry == 0)
                 keepGoing = false;
             else {
