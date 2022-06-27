b47a03e9cc59449ee2e55277ecb4ca5652758f6e
Andrew Taylor <rolyata47@gmail.com>
2022-06-27 08:50:28 -0500

    Adding problem1

---
+++ problem1.cs
@@ -0,0 +1,21 @@
+using System;
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = i+1; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[2] {i, j};
+            }
+        }
+        return null;
+    }
+    static void Main(string[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[4] {2,7,11,15};
+        int target = 9;
+        int[] answer = solution.TwoSum(nums, target);
+        int i = answer[0];
+        int j = answer[1];
+        Console.WriteLine($"i={i} j={j}");
+    }
+}
